6f23161301c82f02e6673a7f961bdfcdd46eddf4
Smolldo <maximqwerbich@gmail.com>
2021-11-04 23:18:32 +0200

    56%

--- src/js/game-scripts.js
+++ src/js/game-scripts.js
@@ -1,43 +1,48 @@
-const refs = {
-    generateBtn: document.querySelector('.generate_btn'),
-    numberSpanPlayer: document.querySelector('.num_now_pl'),
-    numberSpanComputer: document.querySelector('.num_now_ai'),
-    healthBarPlayer: document.querySelector('.round_list_player'),
-    healthBarComputer: document.querySelector('.round_list_computer')
-}
 
-const GENER = () => {
+    const refs = {
+        generateBtn: document.querySelector('.generate_btn'),
+        numberSpanPlayer: document.querySelector('.num_now_pl'),
+        numberSpanComputer: document.querySelector('.num_now_ai'),
+        healthBarPlayer: document.querySelector('.round_list_player'),
+        healthBarComputer: document.querySelector('.round_list_computer'),
+        //liComp: document.querySelector('.player_round_box'),
+        liComp: document.querySelector('.comp_round_box'),
+        winWeb: document.querySelector('.backdrop_win'),
+        looseWeb: document.querySelector('.backdrop_loose'),
+        bossBtn: document.querySelector('.boss_fight_btn'),
+        retryBTN: document.querySelector('.retry')
+    }
+    /////
+    let item = document.createElement('li');
+    item.classList.add('player_round_box');
+    item.textContent = 34;
+    refs.healthBarPlayer.appendChild(item);
+    ////
     
-    let a = Math.floor(Math.random() * 1000);
-    let b  = Math.floor(Math.random() * 1000);
-    refs.numberSpanPlayer.innerHTML = a;
-    refs.numberSpanComputer.innerHTML =b
-        if(a > b){
-            DeleteComp();
-        }else{
-            DeletePlayer();  
+    const generator = () =>{
+        setTimeout(()=>{
+            let a = getRandInt(900);
+        let b = getRandInt(900);
+
+        refs.numberSpanPlayer.textContent = a;
+        refs.numberSpanComputer.textContent = b;
+        if(a < b){
+            item.cloneNode(true);
         }
-
-    };
-
-
-const DeletePlayer = () =>{
-    if(refs.healthBarPlayer.childElementCount == 0){
-        alert("U Loose");
-        refs.healthBarPlayer = false;
+        else{
+            refs.healthBarPlayer.appendChild(item);
+        }
+        },400)
     }
-    else{
-        refs.healthBarPlayer.removeChild(refs.healthBarPlayer.lastChild)
+    
+    const getRandInt = (x) =>{
+        return Math.floor(Math.floor(x) * Math.random())
     }
-}
 
-const DeleteComp = () =>{
-    if(refs.healthBarComputer.childElementCount == 0){
-        alert("U WIN")
-        refs.numberSpanComputer = false;
+    const RETRY = () =>{
+        window.location.reload();
     }
-    else{
-        refs.healthBarComputer.removeChild(refs.healthBarComputer.lastChild)
-    }
-}
-refs.generateBtn.addEventListener('click', GENER);
+
+    refs.retryBTN.addEventListener('click', RETRY);
+    refs.generateBtn.addEventListener('click', generator);
+
