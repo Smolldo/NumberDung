b0c7a4fe4d5a3085f6f0a1a7473af4d9cf29c624
Smolldo <maximqwerbich@gmail.com>
2021-11-06 13:26:22 +0200

    60%

--- src/js/game-scripts.js
+++ src/js/game-scripts.js
@@ -10,29 +10,66 @@
         winWeb: document.querySelector('.backdrop_win'),
         looseWeb: document.querySelector('.backdrop_loose'),
         bossBtn: document.querySelector('.boss_fight_btn'),
-        retryBTN: document.querySelector('.retry')
+        retryBTN: document.querySelector('.retry'),
+        hitSound: document.querySelector('.hit')
     }
-    /////
-    let item = document.createElement('li');
+    /////<a href="https://files.fm/u/kcjykfzcz#/view/hp.jpg"><img src="https://files.fm/thumb_show.php?i=ybwxand52"></a>
+    //<a href="https://files.fm/u/kcjykfzcz#/view/hp.jpg"><img src="https://files.fm/thumb_show.php?i=uupdvbcvj"></a>
+    //<a href="https://files.fm/f/qz6h7drxh"><img src="https://files.fm/thumb_show.php?i=qz6h7drxh"></a>
+    const HP ={
+        src: 'https://files.fm/thumb_show.php?i=ybwxand52',
+        description: "Hit Point",
+    };
+
+    
+    /*let item = document.createElement('li');
     item.classList.add('player_round_box');
     item.textContent = 34;
-    refs.healthBarPlayer.appendChild(item);
+    refs.healthBarPlayer.appendChild(item);*/
+    refs.healthBarPlayer.innerHTML = `<li class="round_item">
+    <span class="player_round_box"><img class="hp" src="${HP.src}" alt="${HP.description}"></span>
+</li>
+<li class="round_item">
+    <span class="player_round_box"><img class="hp" src="${HP.src}" alt="${HP.description}"></span>
+</li>
+<li class="round_item">
+    <span class="player_round_box"><img class="hp" src="${HP.src}" alt="${HP.description}"></span>
+</li>`
+    refs.healthBarComputer.innerHTML = `<li class="round_item">
+    <span class="comp_round_box"><img class="hp" src="${HP.src}" alt="${HP.description}"></span>
+</li>
+<li class="round_item">
+    <span class="comp_round_box"><img class="hp" src="${HP.src}" alt="${HP.description}"></span>
+</li>
+<li class="round_item">
+    <span class="comp_round_box"><img class="hp" src="${HP.src}" alt="${HP.description}"> </span>
+</li>`
     ////
     
     const generator = () =>{
+        refs.hitSound.play();
         setTimeout(()=>{
             let a = getRandInt(900);
         let b = getRandInt(900);
-
         refs.numberSpanPlayer.textContent = a;
         refs.numberSpanComputer.textContent = b;
         if(a < b){
-            item.cloneNode(true);
+            setTimeout(()=>{
+                refs.healthBarPlayer.removeChild(refs.healthBarPlayer.lastElementChild);
+                   if(refs.healthBarPlayer.childElementCount < 1){
+                       refs.looseWeb.classList.remove('is-end');
+                   }
+            }, 600)
         }
         else{
-            refs.healthBarPlayer.appendChild(item);
+            setTimeout(()=>{
+                refs.healthBarComputer.removeChild(refs.healthBarComputer.lastElementChild);
+                if(refs.healthBarComputer.childElementCount < 1){
+                    refs.winWeb.classList.remove('is-end');
+                }
+            }, 600)
         }
-        },400)
+        },600)
     }
     
     const getRandInt = (x) =>{
